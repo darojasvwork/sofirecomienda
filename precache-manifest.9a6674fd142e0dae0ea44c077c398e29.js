b84322171a6169b129a7e5292e5ee5d5455f84bd
darojasvwork <darojasv@gmail.com>
2020-07-15 06:14:19 +0200

    Updates

---
+++ precache-manifest.9a6674fd142e0dae0ea44c077c398e29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72dea9c255a18e654cb6a5de2f303982",
+    "url": "/sofirecomienda/index.html"
+  },
+  {
+    "revision": "494ff43fb36eca818408",
+    "url": "/sofirecomienda/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ffa489a6297102b419b9",
+    "url": "/sofirecomienda/static/js/2.da433722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sofirecomienda/static/js/2.da433722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "494ff43fb36eca818408",
+    "url": "/sofirecomienda/static/js/main.aa4b15a8.chunk.js"
+  },
+  {
+    "revision": "456bb595caab135c95a3",
+    "url": "/sofirecomienda/static/js/runtime-main.cce61113.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sofirecomienda/static/media/logo.5d5d9eef.svg"
+  }
+]);
