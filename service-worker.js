38fdca8f0022f43ad95652b08c2ff64577e1ca1b
darojasvwork <darojasv.work@gmail.com>
2021-07-19 00:40:38 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b91a3abc6a13434256177b47301103ec.js"
+  "/precache-manifest.a973e5eddf450711f8d8bc13999f0b58.js"
 );
 
 self.addEventListener('message', (event) => {
